2c428a682ff9b1540a813fece5706eb3457e4ae5
TubaGuneri <t_guneri@hotmail.com>
2023-12-23 21:24:29 +0100

    opdracht 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
-
+import axios from "axios";
+import {useState} from "react";
+// tijdelijke button maken
+// Maak een request naar de backend om alle posts op te halen
 function App() {
+const [result,setResult]= useState([])
+    async function posts(){
+
+        try {
+
+            const result1 = await axios.get('http://localhost:3000/posts');
+            debugger;
+            setResult(result1);
+            console.log(result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
   return (
     <>
-      <h1>Programmeer hier jouw applicatie</h1>
+      <button type= 'button' onClick={posts}>Alle posts</button>
     </>
   )
 }
